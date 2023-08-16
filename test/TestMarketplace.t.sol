b01ac2dd8131dabcb8c15de8e9c8b163f754e731
usmanrahim <usmanrahim2000@gmail.com>
2023-08-16 21:57:25 +0500

    save

--- test/TestMarketplace.t.sol
+++ test/TestMarketplace.t.sol
@@ -17,14 +17,28 @@ contract TestMarketPlace is Test{
         vm.prank(address(2));
         marketplace.createToken("tokenUri",0.01 ether);
         assertEq(marketplace._tokenIds(),2);
-        assertEq(marketplace.idToListedToken[1].owner,address(1));
+        // assertEq(marketplace.idToListedToken[1].owner,address(1));
+
+        // Here, changing the address to address(3) but it has 0 ether. So, giving it 2 ethers by vm.deal .
+
+        // vm.prank(address(3));
+        // vm.deal(address(3),2 ether);
+
+        // It is working until i do not call executeSale method. When i call it, the prank function reverts.
+
+        address alice = makeAddr("alice");
+        vm.deal(alice,2 ether);
+        vm.prank(alice);
+        marketplace.executeSale{value: 0.01 ether}(1);
+        // assertEq(marketplace._itemsSold(),1);
+        assertEq(marketplace.balanceOf(alice),0);
     }
 
-    function testFailLowListPrice() public{
-        vm.prank(address(1));
-        marketplace.createToken("tokenUri",0.001 ether);
+    // function testFailLowListPrice() public{
+    //     vm.prank(address(1));
+    //     marketplace.createToken("tokenUri",0.001 ether);
 
-    }
+    // }
 
     
 }
---
+++ test/TestNewNftMarketplace.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "../src/NewNftMarketplace.sol";
+
+contract TestNFTMarket is Test{
+    NFTMarket nftmarket;
+
+    function setUp() public{}
+}
